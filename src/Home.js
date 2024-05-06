ca138b5775c923d12c13e0716a791502f4858217
ZiadHegazy99 <ziad.hegazy@student.guc.edu.eg>
2024-05-06 19:39:07 +0300

    updates

--- src/Home.js
+++ src/Home.js
@@ -5,16 +5,57 @@ export function Home() {
     const [question, setQuestion] = useState("");
     const [questions, setQuestions] = useState([]);
     const [answers, setAnswers] = useState([]);
+    const [questionTemp,setQuestionTemp]=useState("");
+    const [answerTemp,setAnswerTemp]=useState([]);
+    const [letterIndex,setLetterIndex]=useState(0);
+    const [answerIndex,setAnswerIndex]=useState(0);
     const handleQuestionChange=(e)=>{
         setQuestion(e.target.value);
     }
     const handleQuestionSubmit=()=>{
-        setQuestions([...questions,question]);
-        setQuestion("");
-        setAnswers([...answers,["I am a chatbot, I am here to help you with your queries.","Doctor mohamed will be available today at 10pm"]]);
-        setLines(["I am a chatbot, I am here to help you with your queries.","Doctor mohamed will be available today at 10pm"]);
+        const lines=["Doctor Amr will be available today at 10pm","Anything else you want to ask?"]
+        setLines(lines);
+        setAnswerTemp(lines);
+        setQuestionTemp(question);
+        handleLineByLine(lines,question);
+    }
+    const handleLineByLine=(lines2,questionTemp2)=>{
+        var letterIndex2=letterIndex;
+        var answerIndex2=answerIndex;
+        var timer=0;
+        var interval=setInterval(()=>{
+            timer+=1;
+            if(timer%2.25==0){
+                document.getElementById("lastQ1")?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            }
+
+            if(lines2.length>0 &&  letterIndex2<lines2[answerIndex2].length){
+                setLetterIndex(letterIndex=>letterIndex+1);
+                letterIndex2+=1;
+            }
+            else{
+                setLetterIndex(0);
+                letterIndex2=0;
+                answerIndex2+=1;
+                setAnswerIndex(answerIndex=>answerIndex+1);
+            }
+            if(answerIndex2>=lines2.length){
+                clearInterval(interval);
+                setQuestions(questions=>[...questions,questionTemp2]);
+                setQuestion("");
+                setAnswers(answers=>[...answers,lines2]);
+                setQuestionTemp("");
+                setAnswerTemp([]);
+                setAnswerIndex(0);
+                setLetterIndex(0);
+                
+        
+            }
+        },40);
+
     }
     useEffect(()=>{
+        console.log(questions);
         document.getElementById("lastQ")?.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }, [questions]);
     
@@ -43,6 +84,26 @@ export function Home() {
                      </div>)
             })
         }
+       {questionTemp!="" &&  <div  className="QA"> 
+                    <div className="question">
+                    <label>You:</label>
+                    <br></br>
+                    <label>{questionTemp}</label>
+                </div>
+                <br></br>
+                <div id="lastQ1" className="answer" >
+                    <label>Chatbot:</label>
+                    <ul>
+                        {answerTemp.slice(0,answerIndex+1).map((answer,index)=>{
+                            if(index==answerIndex){
+                                return <li>{answer.substring(0,letterIndex)}</li>
+                            }else{
+                                return <li>{answer}</li>
+                            }
+                        })}
+                    </ul>
+                </div>
+         </div>}
         
       </div>
       <br></br>
